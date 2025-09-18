cb1f63aa3ca8c588d7f7e8086b1a91071a5bd55d
rafafls <rafaelngflausino@gmail.com>
2025-09-18 08:38:57 -0300

    .

---
+++ logica_programacao-main/Modulo2-JavaScript/Aula11-Variaveis e DOM/Variaveis/script.js
@@ -0,0 +1,17 @@
+console.log("String" + "Hello Word");
+console.log("Número" + (2 + 2));
+console.log("Booleano" + true);
+console.log("Olá, Mundo!!!");
+
+// var
+var nome = "Matheus"
+nome = "Robson"
+console.log(nome)
+
+//let
+let sobrenome = "Souza"
+sobrenome = "Silva"
+
+
+//Constante
+const pi = 3.14
